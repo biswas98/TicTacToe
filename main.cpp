1476a859b93fc36ba22b480d979a467fadac6d68
biswas98 <biswasbiplab98@gmail.com>
2022-07-08 18:28:05 +0530

    Initial Program for TicTacToe

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    
+
+    int blah;
+    blah=1;
+    cout<<blah<<endl;
+    blah='x';
+    cout<<blah;
+    
+    
+
+    return 0;
+}
